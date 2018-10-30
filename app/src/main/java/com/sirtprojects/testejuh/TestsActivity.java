039c0797260190f18261aad0c07eeed8593dd830
Felipe Campos <felipecamposifce@gmail.com>
2018-10-30 16:30:51 -0300

    Adição do TestsActivity + Strings

--- app/src/main/java/com/sirtprojects/testejuh/TestsActivity.java
+++ app/src/main/java/com/sirtprojects/testejuh/TestsActivity.java
@@ -1,15 +1,43 @@
 package com.sirtprojects.testejuh;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.led_on_off.led.R;
 
-public class TestsActivity extends Activity {
+public class TestsActivity extends Activity implements View.OnClickListener {
+
+    Button btComandos, btListaDispositivos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tests);
+
+        btComandos = findViewById(R.id.button_tests_comandos);
+        btListaDispositivos = findViewById(R.id.button_tests_lista_dispositivos);
+
+        btComandos.setOnClickListener(this);
+        btListaDispositivos.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v != null){
+            if(v.getId() == btComandos.getId()){
+                Intent intent = new Intent(TestsActivity.this, ledControl.class);
+                startActivity(intent);
+            }
+            else if(v.getId() == btListaDispositivos.getId()){
+                Intent intent = new Intent(TestsActivity.this, DeviceList.class);
+                startActivity(intent);
+            }
+        }
     }
 }
