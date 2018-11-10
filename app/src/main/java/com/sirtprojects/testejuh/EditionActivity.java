c62a8afe0c1be2a2e153d6a0f808487587e02e1d
Felipe Campos <felipecamposifce@gmail.com>
2018-11-10 19:43:25 -0300

    Update

---
+++ app/src/main/java/com/sirtprojects/testejuh/EditionActivity.java
@@ -0,0 +1,15 @@
+package com.sirtprojects.testejuh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.led_on_off.led.R;
+
+public class EditionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edition);
+    }
+}
